bca9a63bf3ebf4919e4e1c6610063c3e126e1fef
UerLink <UerLink@163.com>
2022-05-08 06:30:29 +0800

    Site updated: 2022-05-08 06:30:28

--- 新建文章并打开.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p name=input page name:
-echo name:%name%
-echo please wait
-hexo new %name%  && call typora.bat %name%
-pause
--- 渲染博客.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd  C:\SaveData\GitHubWeb\UerLinkBlog
-hexo clean
-hexo g
-pause
