ac1baed9e46e816adf7257c25c522e795f2a0be1
UerLink <UerLinkGit@outlook.com>
2023-08-02 15:58:23 +0800

    Site updated: 2023-08-02 15:58:22

--- 提交到GitHub.bat
+++ 提交到GitHub.bat
@@ -2,8 +2,12 @@
 cd /d  D:\UerLinkBlog
 title HexoGo
 cd 
-call hexo clean
+echo "清除缓存已禁用...如果博客样式出现问题，请重新开启"
+%call hexo clean%
+echo "博客开始重新生成:"
 call hexo g
+echo "博客开始上传GitHub:"
 call hexo d
+echo "博客预览服务启动:"
 call hexo s
 pause
